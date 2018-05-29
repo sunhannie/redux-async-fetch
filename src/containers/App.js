5a629d36f03b9c345aeb9379c7d582c060273eba
huiyun.he <huiyun@huiyunhedeMacBook-Pro.local>
2018-05-29 23:05:42 +0800

    create components

---
+++ src/containers/App.js
@@ -0,0 +1,107 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import { selectSubreddit, fetchPostsIfNeeded, invalidateSubreddit } from '../actions'
+import Picker from '../components/Picker'
+import Posts from '../components/Posts'
+
+class App extends Component {
+  static propTypes = {
+    selectedSubreddit: PropTypes.string.isRequired,
+    posts: PropTypes.array.isRequired,
+    isFetching: PropTypes.bool.isRequired,
+    lastUpdated: PropTypes.number,
+    dispatch: PropTypes.func.isRequired
+  }
+
+//this.props为：{selectedSubreddit: "reactjs", posts: Array(0), isFetching: true, lastUpdated: undefined, dispatch: ƒ}
+  componentDidMount() {
+    
+    const { dispatch, selectedSubreddit } = this.props
+    dispatch(fetchPostsIfNeeded(selectedSubreddit))
+  }
+
+/*
+this.props:开始获得2次
+{selectedSubreddit: "reactjs", posts: Array(0), isFetching: true, lastUpdated: undefined, dispatch: ƒ}
+{selectedSubreddit: "reactjs", posts: Array(0), isFetching: true, lastUpdated: undefined, dispatch: ƒ}
+
+nextProps:开始获得2次
+{selectedSubreddit: "reactjs", posts: Array(0), isFetching: true, lastUpdated: undefined, dispatch: ƒ}
+{selectedSubreddit: "reactjs", posts: Array(26), isFetching: false, lastUpdated: 1527585247618, dispatch: ƒ}
+*/
+  componentWillReceiveProps(nextProps) {
+    console.log(this.props);  
+    if (nextProps.selectedSubreddit !== this.props.selectedSubreddit) {
+      const { dispatch, selectedSubreddit } = nextProps
+      dispatch(fetchPostsIfNeeded(selectedSubreddit))
+    }
+  }
+
+  handleChange = nextSubreddit => {
+    this.props.dispatch(selectSubreddit(nextSubreddit))
+  }
+
+  handleRefreshClick = e => {
+    e.preventDefault()
+
+    const { dispatch, selectedSubreddit } = this.props
+    dispatch(invalidateSubreddit(selectedSubreddit))
+    dispatch(fetchPostsIfNeeded(selectedSubreddit))
+  }
+
+  render() {
+    const { selectedSubreddit, posts, isFetching, lastUpdated } = this.props
+    const isEmpty = posts.length === 0
+    return (
+      <div>
+        <Picker value={selectedSubreddit}
+                onChange={this.handleChange}
+                options={[ 'reactjs', 'frontend' ]} />
+        <p>
+          {lastUpdated &&
+            <span>
+              Last updated at {new Date(lastUpdated).toLocaleTimeString()}.
+              {' '}
+            </span>
+          }
+          {!isFetching &&
+            <button onClick={this.handleRefreshClick}>
+              Refresh
+            </button>
+          }
+        </p>
+        {isEmpty
+          ? (isFetching ? <h2>Loading...</h2> : <h2>Empty.</h2>)
+          : <div style={{ opacity: isFetching ? 0.5 : 1 }}>
+              <Posts posts={posts} />
+            </div>
+        }
+      </div>
+    )
+  }
+}
+// 我添加testProp，不管我有没有赋值，初始获取得还是undefined，这是因为Redux store没有发生变化的原因吗？
+
+const mapStateToProps = state => {
+  const {testProp} = {'test':'1'}; //或写数组['aa']
+  const { selectedSubreddit, postsBySubreddit } = state
+  const {
+    isFetching,
+    lastUpdated,
+    items: posts
+  } = postsBySubreddit[selectedSubreddit] || {
+    isFetching: true,
+    items: []
+  }
+
+  return {
+    selectedSubreddit,
+    posts,
+    isFetching,
+    lastUpdated,
+    testProp
+  }
+}
+
+export default connect(mapStateToProps)(App)
---
+++ src/reducers/index.js
@@ -0,0 +1,69 @@
+/**
+ * 先写好这些代码，最后用一个固定值来渲染页面
+ */
+import { combineReducers } from 'redux'
+import {
+  SELECT_SUBREDDIT, INVALIDATE_SUBREDDIT,
+  REQUEST_POSTS, RECEIVE_POSTS
+} from '../actions'
+
+const selectedSubreddit = (state = 'reactjs', action) => {
+    switch (action.type){
+        case SELECT_SUBREDDIT:
+            return action.subreddit
+        default:
+            return state
+    }
+}
+
+// 新的state，根据需要自己添加属性
+const posts = (state = {
+    isFetching:false,
+    didInvalidate:false,
+    items:[]
+},action) => {
+    switch (action.type) {
+        case INVALIDATE_SUBREDDIT:
+            return{
+                ...state,
+                didInvalidate:true
+            }
+        case REQUEST_POSTS:
+            return{
+                ...state,
+                isFetching:true,
+                didInvalidate:false
+            }
+        case RECEIVE_POSTS:
+            return {
+                ...state,
+                isFetching: false,
+                didInvalidate: false,
+                items: action.posts,
+                lastUpdated: action.receivedAt
+            }
+        default:
+            return state
+    }
+}
+
+const postsBySubreddit = (state = { }, action) => {
+  switch (action.type) {
+    case INVALIDATE_SUBREDDIT:
+    case RECEIVE_POSTS:
+    case REQUEST_POSTS:
+      return {
+        ...state,
+        [action.subreddit]: posts(state[action.subreddit], action)
+      }
+    default:
+      return state
+  }
+}
+// 结合起来，返回一对象
+const rootReducer = combineReducers({
+  postsBySubreddit,
+  selectedSubreddit
+})
+
+export default rootReducer
